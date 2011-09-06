2b86e55c4c1d5a71a2d8713cb5892971f5596ec6
Sam Bromley <sam@sambromley.com>
2011-09-06 14:32:36 -0230

    Remove cruft before pushing to github.

--- src/tclArgv.c
+++ src/tclArgv.c
@@ -10,7 +10,7 @@
  * 	Copyright (c) 1990-1994 The Regents of the University of California.
  * 	Copyright (c) 1994-1997 Sun Microsystems, Inc.
  *
- * See the file "license.terms" for information on usage and redistribution
+ * See the file "COPYING" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
  */
